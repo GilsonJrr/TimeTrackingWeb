88915892c9d827c80751701416429591432b0fef
GilsonJrr <gilson99cosme@gmail.com>
2021-12-19 16:08:54 -0300

    Cards n  Imgs

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import './App.css';
 
+import Work from '../src/Image/Work.png'
+import Play from '../src/Image/Play.png'
+import Study from '../src/Image/Study.png'
+import Exercise from '../src/Image/Exercise.png'
+import Social from '../src/Image/Social.png'
+import SelfCare from '../src/Image/Self-care.png'
+
 import MainCard from './Pages/MainCard';
 import Card from './Pages/Cards';
 
@@ -9,16 +16,16 @@ function App() {
     <body className="App">
       <MainCard/>
       <div>
-        <Card/>
-        <Card/>
+        <Card title="Work" color="#FD8C64" img={Work} time="32"/>
+        <Card title="Exercise" color="#4BCF83" img={Exercise} time="4"/>
       </div>
       <div>
-        <Card/> 
-        <Card/>
+        <Card title="Play" color="#56C2E6" img={Play} time="10"/> 
+        <Card title="Social" color="#7235D1" img={Social} time="5"/>
       </div>
       <div>
-        <Card/>
-        <Card/>
+        <Card title="Study" color="#FF5E7D" img={Study} time="4"/>
+        <Card title="Self Care" color="#F1C75B" img={SelfCare} time="2"/>
       </div>
     </body>
   );
--- src/Pages/Cards.js
+++ src/Pages/Cards.js
@@ -1,10 +1,28 @@
 import React from 'react';
 import '../App.css';
 
+import Dots from '../Image/Dots.png';
+
 function Card(props) {
   return (
-    <div className="CardMain">
-        <div className="Card"> </div>
+    <div className="CardMain" style={{backgroundColor: props.color }}>
+
+        <img src={props.img} />
+
+        <div className="Card">
+
+          <div> 
+            <div className="Activitie"> {props.title} </div>
+            <div className="Hours">{props.time}hrs</div>
+            <div className="Past">Last week - 36hrs</div>
+          </div> 
+
+          <div className="Img">
+            <img src={Dots} alt="Dots"/>
+          </div>
+
+        </div>
+        
     </div>
   );
 }
