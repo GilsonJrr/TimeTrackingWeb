5bab656bafab94b0efb2fffefa5f4d98a58964a1
GilsonJrr <gilson99cosme@gmail.com>
2021-12-20 00:45:50 -0300

    Data done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
 import React, {useState} from 'react';
 import './App.css';
 
-import Work from '../src/Image/Work.png'
-import Play from '../src/Image/Play.png'
-import Study from '../src/Image/Study.png'
-import Exercise from '../src/Image/Exercise.png'
-import Social from '../src/Image/Social.png'
-import SelfCare from '../src/Image/Self-care.png'
-
 import MainCard from './Pages/MainCard';
 import Card from './Pages/Cards';
+import { data } from './Data/Data';
 
 function App() {
 
@@ -17,15 +11,6 @@ function App() {
   const [ week, setWeek] = useState (false)
   const [ month, setMonth] = useState (false)
   const [select, setSelect] = useState ('day');
-
-  const [data, setData] = useState ([
-    {id: 1, Title: 'Work', Color: '#FD8C64', Time: {day: 8, week: 40, month: 160}, Image: Work },
-    {id: 2, Title: 'Play', Color: '#56C2E6', Time: {day: 2, week: 14, month: 56}, Image: Play },
-    {id: 3, Title: 'Study', Color: '#FF5E7D', Time: {day: 2, week: 14, month: 56}, Image: Study },
-    {id: 4, Title: 'Exercise', Color: '#4BCF83', Time: {day: 2, week: 14, month: 56}, Image: Exercise },
-    {id: 5, Title: 'Social', Color: '#7235D1', Time: {day: 2, week: 14, month: 56}, Image: Social },
-    {id: 6, Title: 'Self Care', Color: '#F1C75B', Time: {day: 8, week: 56, month: 152}, Image: SelfCare },
-  ])
 
   function HandleDay (){
     setDay(true); setWeek(false); setMonth(false); setSelect('day')
---
+++ src/Data/Data.js
@@ -0,0 +1,16 @@
+
+import Work from '../Image/Work.png'
+import Play from '../Image/Play.png'
+import Study from '../Image/Study.png'
+import Exercise from '../Image/Exercise.png'
+import Social from '../Image/Social.png'
+import SelfCare from '../Image/Self-care.png'
+
+export const data = ([
+    {id: 1, Title: 'Work', Color: '#FD8C64', Time: {day: 8, week: 40, month: 160}, Image: Work },
+    {id: 2, Title: 'Play', Color: '#56C2E6', Time: {day: 2, week: 14, month: 56}, Image: Play },
+    {id: 3, Title: 'Study', Color: '#FF5E7D', Time: {day: 2, week: 14, month: 56}, Image: Study },
+    {id: 4, Title: 'Exercise', Color: '#4BCF83', Time: {day: 2, week: 14, month: 56}, Image: Exercise },
+    {id: 5, Title: 'Social', Color: '#7235D1', Time: {day: 2, week: 14, month: 56}, Image: Social },
+    {id: 6, Title: 'Self Care', Color: '#F1C75B', Time: {day: 8, week: 56, month: 152}, Image: SelfCare },
+  ])
