4d27d94335a4f200972df2450be6f8b175381470
GilsonJrr <gilson99cosme@gmail.com>
2021-12-20 00:38:17 -0300

    day week month time done

--- src/App.js
+++ src/App.js
@@ -16,43 +16,52 @@ function App() {
   const [ day, setDay] = useState (true)
   const [ week, setWeek] = useState (false)
   const [ month, setMonth] = useState (false)
+  const [select, setSelect] = useState ('day');
+
+  const [data, setData] = useState ([
+    {id: 1, Title: 'Work', Color: '#FD8C64', Time: {day: 8, week: 40, month: 160}, Image: Work },
+    {id: 2, Title: 'Play', Color: '#56C2E6', Time: {day: 2, week: 14, month: 56}, Image: Play },
+    {id: 3, Title: 'Study', Color: '#FF5E7D', Time: {day: 2, week: 14, month: 56}, Image: Study },
+    {id: 4, Title: 'Exercise', Color: '#4BCF83', Time: {day: 2, week: 14, month: 56}, Image: Exercise },
+    {id: 5, Title: 'Social', Color: '#7235D1', Time: {day: 2, week: 14, month: 56}, Image: Social },
+    {id: 6, Title: 'Self Care', Color: '#F1C75B', Time: {day: 8, week: 56, month: 152}, Image: SelfCare },
+  ])
 
   function HandleDay (){
-    setDay(true); setWeek(false); setMonth(false)
+    setDay(true); setWeek(false); setMonth(false); setSelect('day')
   }
 
   function HandleWeek (){
-    setDay(false); setWeek(true); setMonth(false)
+    setDay(false); setWeek(true); setMonth(false); setSelect('week')
   }
 
   function HandleMonth (){
-    setDay(false); setWeek(false); setMonth(true);
+    setDay(false); setWeek(false); setMonth(true); setSelect('month')
   }
 
   return (
     <body className="App">
 
-      <MainCard 
-        day={day} 
-        HandleDay={HandleDay}
-        week={week} 
-        HandleWeek={HandleWeek}
-        month={month}
-        HandleMonth={HandleMonth}
-      />
-
-      <div>
-        <Card title="Work" color="#FD8C64" img={Work} time="32" day={day} week={week} month={month} />
-        <Card title="Exercise" color="#4BCF83" img={Exercise} time="4" day={day} week={week} month={month} />
-      </div>
-      <div>
-        <Card title="Play" color="#56C2E6" img={Play} time="10" day={day} week={week} month={month} /> 
-        <Card title="Social" color="#7235D1" img={Social} time="5" day={day} week={week} month={month} />
-      </div>
-      <div>
-        <Card title="Study" color="#FF5E7D" img={Study} time="4" day={day} week={week} month={month} />
-        <Card title="Self Care" color="#F1C75B" img={SelfCare} time="2" day={day} week={week} month={month} />
-      </div>
+        <MainCard 
+          day={day} 
+          HandleDay={HandleDay}
+          week={week} 
+          HandleWeek={HandleWeek}
+          month={month}
+          HandleMonth={HandleMonth}
+        />
+      
+    
+        <div className="ContainerCards">
+          {data.map((item) => {
+            return(
+              <div key={item.id}>
+                <Card title={item.Title} color={item.Color} img={item.Image} time={item.Time[select]} day={day} week={week} month={month} />
+              </div>
+            );
+          })}
+        </div>
+      
     </body>
   );
 }
